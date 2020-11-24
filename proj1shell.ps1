735b9a767b3106137ab863d839f8edbfa23aebc8
aditipatil138 <55366437+aditipatil138@users.noreply.github.com>
2020-11-24 15:17:40 -0800

    Add files via upload

---
+++ proj1shell.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host 'Hello World!!'
+git --version
+git clone https://github.com/aditipatil138/Web-Desigining.git 
+start .\proj1.html
+Write-Host 'End of Script!'
---
+++ proj4shell.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host '---------------Hello Start of the script!!-----------------'
+flutter emulators --launch Pixel_2_API_30
+git --version
+git clone https://github.com/shriyashwarghade/QUIZ-APP-FLUTTER.git
+cd QUIZ-APP-FLUTTER
+flutter packages get
+flutter pub get
+flutter doctor --android-licenses
+flutter config --android-sdk C:\Users\aditi\AppData\Local\Android\Sdk
+flutter doctor
+flutter run
+Write-Host '------------------------cleaning up-----------------------------!'
