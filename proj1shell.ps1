f67b822fa394677066c5a0a7ab310b3aae7527ed
aditipatil138 <55366437+aditipatil138@users.noreply.github.com>
2020-11-24 15:18:49 -0800

    Update proj1shell.ps1

--- proj1shell.ps1
+++ proj1shell.ps1
@@ -1,4 +1,5 @@
-﻿Write-Host 'Hello World!!'
+//This shell script to run a web design page on a browser
+Write-Host 'Hello World!!'
 git --version
 git clone https://github.com/aditipatil138/Web-Desigining.git 
 start .\proj1.html
